ab234fcdfd9abe097faad3045c0879798faa2041
Haoran Feng <harrifeng@gmail.com>
2020-07-05 21:56:51 +0800

    new ok

---
+++ pick.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+from shutil import copyfile
+
+target_dir = "."
+
+pathlist = Path(target_dir).glob("**/*.md")
+
+for path in pathlist:
+    src = str(path)
+    dest = os.path.join("./md", src.split("/")[0] + ".md")
+    print(src, dest)
+
+
+
