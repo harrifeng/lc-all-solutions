d66e007c8188a015d5034c39b1dd167de4ed8ec8
Haoran Feng <harrifeng@gmail.com>
2020-07-05 22:15:44 +0800

    new ok

--- cut.py
+++ cut.py
@@ -24,4 +24,5 @@ if __name__ == "__main__":
     pathlist = Path(target_dir).glob("**/*.py")
 
     for path in pathlist:
-        change_target_file(path)
+        print(path)
+        # change_target_file(path)
